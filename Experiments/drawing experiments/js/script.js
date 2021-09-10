3d76dc183082b1e6dae26fd661744eff6fa9d05e
DestinyChescappio <90113915+DestinyChescappio@users.noreply.github.com>
2021-09-09 21:23:35 -0400

    drew a target drawing experiment

--- Experiments/drawing experiments/js/script.js
+++ Experiments/drawing experiments/js/script.js
@@ -14,12 +14,11 @@ function setup() {
   createCanvas(500,500);
 
 background(200,200,200);
-
-point(250,250);
-rect(0,0,250,250);
-line(500,0,0,500);
-line(0,0,500,500);
+ellipse(CORNER);
 ellipse(250,250,100,100);
+ellipse(250,250,80,80);
+ellipse(250,250,60,60);
+ellipse(250,250,40,40);
 }
 
 // draw()
