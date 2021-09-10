dabc9837143c647bde13900d56e718a69af05f5e
DestinyChescappio <90113915+DestinyChescappio@users.noreply.github.com>
2021-09-09 21:07:17 -0400

    drawing experiment drew a cone

--- Experiments/drawing experiments/js/script.js
+++ Experiments/drawing experiments/js/script.js
@@ -11,6 +11,15 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+
+background(200,200,200);
+
+point(250,250);
+rect(0,0,250,250);
+line(500,0,0,500);
+line(0,0,500,500);
+ellipse(250,250,100,100);
 }
 
 // draw()
