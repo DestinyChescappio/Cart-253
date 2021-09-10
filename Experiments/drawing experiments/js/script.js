ed3407625e2387e1c5fc1e85955a9cdb39949396
DestinyChescappio <90113915+DestinyChescappio@users.noreply.github.com>
2021-09-09 22:07:32 -0400

    added color to target drawing experiment

--- Experiments/drawing experiments/js/script.js
+++ Experiments/drawing experiments/js/script.js
@@ -13,11 +13,21 @@ Here is a description of this template p5 project.
 function setup() {
   createCanvas(500,500);
 
-background(200,200,200);
+background(84,154,158);
+noStroke();
+
 ellipse(CORNER);
+
+fill(127,0,200,100);
 ellipse(250,250,100,100);
+
+fill(127,0,210,100);
 ellipse(250,250,80,80);
+
+fill(147,0,220,100);
 ellipse(250,250,60,60);
+
+fill(157,0,230,100);
 ellipse(250,250,40,40);
 }
 
