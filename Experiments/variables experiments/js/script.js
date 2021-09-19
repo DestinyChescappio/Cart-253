2c8cd23b44d4aefc2b8556560d3d6ef0c2642b27
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-19 15:39:38 -0400

    learned about creating variables

--- Experiments/variables experiments/js/script.js
+++ Experiments/variables experiments/js/script.js
@@ -1,22 +1,31 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Variables 1.0
+Destiny Chescappio
 
-Here is a description of this template p5 project.
+Intro to variables: creating & changing
 **************************************************/
-
+let backgroundShade = 0;
+let circleX = 0;
+let circleY = -1;
+let circleSize = 200
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
+circleSize=200;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+background(0);
+backgroundShade = backgroundShade + 1;
+backgroundShade;
+circleSize = circleSize * 1.01;
+ellipse(circleX,circleY,circleSize);
 
 }
