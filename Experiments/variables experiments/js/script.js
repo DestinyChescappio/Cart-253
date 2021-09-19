0806c6b8ac2fc1672398369ba8b322107c458cc7
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-19 15:49:50 -0400

    learning about changing variables!

--- Experiments/variables experiments/js/script.js
+++ Experiments/variables experiments/js/script.js
@@ -7,26 +7,24 @@ Destiny Chescappio
 Intro to variables: creating & changing
 **************************************************/
 let backgroundShade = 0;
-let circleX = 250;
-let circleY = 0;
+let circleX = 0;
+let circleY = 250;
 let circleSize = 200;
-let circleAcceleration = 0.25;
+let circleSpeed = 2;
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-createCanvas(windowWidth,windowHeight);
-circleSize=200;
+createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-background(0);
 background(backgroundShade);
-circleX += circlespeed;
-circleSpeed += circleAcceleration;
-ellipse(circleX,circleY,circleSize);
+circleX += circleSpeed;
+ellipse(circleX, circleY, circleSize);
 
 }
