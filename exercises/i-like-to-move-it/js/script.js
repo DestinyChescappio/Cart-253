59f0169a3e4ba344c208dd9eaf2ccab8529b6145
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-20 22:26:36 -0400

    Made background and growing circle shape

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -6,21 +6,37 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-"use strict";
+
+let bg = {
+  r:50,
+  g: 0,
+  b: 80,
+
+};
 
 
-/**
-Description of preload
-*/
-function preload() {
+  let circle1 = {
+    x: 400,
+    y: 400,
+    size: 100,
+    growthRate: 0.5,
+    speed: 1,
+    fill: 190,
+    alpha: 220
 
-}
+
+    };
+    let circleX = 100;
+    let circleY = 100;
+    let circleSize = 200;
 
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(800,800);
+  noStroke();
 
 }
 
@@ -29,5 +45,24 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(bg.r,bg.g,bg.b);
+  bg.b = bg.b + 0;
+
+
+
+//Middle circle1
+  circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x,400,800/2);
+  circle1.size = circle1.size + circle1.growthRate;
+  circle1.size = constrain(circle1.size,0,width);
+  fill(circle1.fill,circle1.alpha);
+  ellipse(circle1.x,circle1.y,circle1.size);
+
+//Circle2
+  circleSize = 50;
+  ellipse(circleX, circleY, circleSize);
+
+
+
 
 }
