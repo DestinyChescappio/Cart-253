cdc29ff85e57b55ce7c2cd275d0447db7658a2bc
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-26 19:03:26 -0400

    experimented with booleans: to make a shape pressing the mouse button

--- Experiments/Conditionals/js/script.js
+++ Experiments/Conditionals/js/script.js
@@ -1,4 +1,4 @@
-
+let displayCircle = false;
 
 function setup() {
   createCanvas(500,500);
@@ -8,12 +8,14 @@ function setup() {
 
 
 function draw() {
-if (keyIsPressed === true) {
-background(255);
-  }
-else {
-  background(0);
-}
 
+  background(0);
+  if (mouseIsPressed) {
+    displayCircle = true;
+  }
+
+  if (displayCircle){
+    ellipse(250,250,100,100);
+  }
 
 }
