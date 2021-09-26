197538ad11211999b490921394abaccebc1705ac
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-26 13:22:39 -0400

    Experimented with if, else if, and otherwise

--- Experiments/Conditionals/js/script.js
+++ Experiments/Conditionals/js/script.js
@@ -1,26 +1,13 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 1
 
 }
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(500,500);
 
 }
 
@@ -29,5 +16,20 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(backgroundShade);
+
+  circle.x = circle.x + circle.speed;
+
+  if (mouseX < width/2) {
+    fill(255,0,0);
+  }
+  else if (mouseX < 2 * width/3){
+    fill(0,255,0);
+  }
+  else {
+    fill(0,0,255);
+  }
+
+  ellipse(circle.x,circle.y,circle.size);
 
 }
