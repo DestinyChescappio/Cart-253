99c6d2b2ce63ba9358a180be038e19ae85861adf
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-28 19:50:14 -0400

    velocity: made circle follow mouse on x axis

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -18,7 +18,7 @@ let circle = {
   x: 0,
   y:350,
   size:100,
-  vx: 1,
+  vx: 0,
   vy: 0
 };
 
@@ -37,7 +37,14 @@ Description of draw()
 */
 function draw() {
   background('#fae');
-
+  // if mouseX is less than the circle X position, mouse positioned to left side
+  if (mouseX < circle.x){
+    circle.vx = -1;
+  }
+  //otherwise, the mouse x position goes to right side 
+  else {
+    circle.vx = 1;
+  }
   //velocity used to make circle go to right side horizontally
   circle.x = circle.x + circle.vx;
   circle.y + circle.y + circle.vy;
