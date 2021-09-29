f99623de1c58c1369e07dab19e0ef2fa185a242b
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-28 20:49:07 -0400

    added velocity through acceleration without velocity maxing speed

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -20,6 +20,10 @@ let circle = {
   size:100,
   vx: 0,
   vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 1,
+  maxSpeed: 10,
   fill: {
     r: 200,
     g: 0,
@@ -42,20 +46,29 @@ Description of draw()
 */
 function draw() {
   background('#fae');
-  // Circle following mouse X- moving from left to right
+  // Circle following mouse on both X & Y axis
   if (mouseX < circle.x) {
-    circle.vx = -1;
+    circle.ax = -circle.acceleration;
   }
   else {
-    circle.vx = 1;
+    circle.ax = circle.acceleration;
   }
 if (mouseY < circle.y){
-  circle.vy = -1;
+  circle.ay = -circle.acceleration;
 }
 else {
-  circle.vy = 1;
+  circle.ay = circle.acceleration;
 }
-  //velocity used to make circle go to right side horizontally
+
+//setting velocity through acceleration & constraining velocity from reaching max speed
+circle.vx = circle.vx + circle.ax;
+circle.vx = constrain(circle.vx,-circle.maxspeed,circle.maxspeed);
+circle.vy = circle.vy + circle.ay;
+circle.vy = constrain(circle.vy, -circle.maxspeed,circle.maxspeed);
+
+
+
+//velocity used to make circle go to right side horizontally
   circle.x = circle.x + circle.vx;
   circle.y = circle.y + circle.vy;
 
