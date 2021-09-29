a6cffe5b68bf5c05f57d16e107e6aa69acf37e72
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-28 23:30:00 -0400

    program to stop when circle overlapping tiny circle: fail
    as much as I tried, I could not manage to have to the program stop when the moving shapes touched and reached Voldemort's nose.

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -15,6 +15,16 @@ let targetObjective = undefined;
 function preload() {
 targetObjective = loadImage('assets/images/no nose.jpeg');
 }
+//tiny black circle
+let tinyCircle = {
+  x: 0,
+  y:285,
+  size:10,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+  fill: 255
+}
 //a circle object
 let circle = {
   x: 300,
@@ -24,27 +34,23 @@ let circle = {
   vy: 0,
   ax: 0,
   ay: 0,
-  acceleration: 0.25,
-  maxSpeed: 5,
+  acceleration: 0.02,
+  maxSpeed: 1,
   fill: {
     r: 150,
     g: 0,
     b: 100
     }
 }
-let tinyCircle = {
-  x: 346,
-  y:285,
-  size:10,
-  fill: 0
-}
-
 
 /**
 Description of setup
 */
 function setup() {
   createCanvas(600,600);
+
+  tinyCircle.y = tinyCircle.y + tinyCircle.X
+  tinyCircle.vx = tinyCircle.speed;
 
 }
 
@@ -59,7 +65,16 @@ function draw() {
   imageMode(CENTER);
   image(targetObjective,width/2,height/2);
 
-//
+
+//tiny circle movement
+tinyCircle.x = tinyCircle.x + tinyCircle.vx;
+tinyCircle.y = tinyCircle.y + tinyCircle.vy;
+
+//tiny circle movement to left
+if (tinyCircle.x > width){
+  tinyCircle.x = 0;
+  tinyCircle.y = 285;
+}
 
   // Circle following mouse on both X & Y axis
   if (mouseX < circle.x) {
@@ -89,14 +104,17 @@ circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
   circle.y = circle.y + circle.vy;
 
 
+
+//display tiny Circle
+  fill(tinyCircle.fill);
+  noStroke();
+  ellipse(tinyCircle.x,tinyCircle.y,tinyCircle.size);
+
 //display circle
 fill(circle.fill.r,circle.fill.g,circle.fill.b);
 noStroke();
 ellipse(circle.x,circle.y,circle.size);
 
-//display tiny Circle
-fill(tinyCircle.fill);
-noStroke();
-ellipse(tinyCircle.x,tinyCircle.y,tinyCircle.size);
+
 
 }
