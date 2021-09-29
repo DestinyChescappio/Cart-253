c4b40c6752a1c85d12518a1735341f761ad9c09a
DestinyChescappio <chescappiodestiny@gmail.com>
2021-09-28 20:24:52 -0400

    changed canvas size and changed circle color

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -15,11 +15,16 @@ function preload() {
 }
 //a circle object
 let circle = {
-  x: 0,
-  y:350,
+  x: 300,
+  y:300,
   size:100,
   vx: 0,
-  vy: 0
+  vy: 0,
+  fill: {
+    r: 200,
+    g: 0,
+    b: 200
+  }
 };
 
 
@@ -27,7 +32,7 @@ let circle = {
 Description of setup
 */
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(600,600);
 
 }
 
@@ -37,22 +42,28 @@ Description of draw()
 */
 function draw() {
   background('#fae');
-  // if mouseX is less than the circle X position, mouse positioned to left side
-  if (mouseX < circle.x){
+  // Circle following mouse X- moving from left to right
+  if (mouseX < circle.x) {
     circle.vx = -1;
   }
-  //otherwise, the mouse x position goes to right side 
   else {
     circle.vx = 1;
   }
+if (mouseY < circle.y){
+  circle.vy = -1;
+}
+else {
+  circle.vy = 1;
+}
   //velocity used to make circle go to right side horizontally
   circle.x = circle.x + circle.vx;
-  circle.y + circle.y + circle.vy;
+  circle.y = circle.y + circle.vy;
 
 
 
-  //draw circle
-ellipse(circle.x,circle.y,circle.size);
+//display circle
+fill(circle.fill.r,circle.fill.g,circle.fill.b);
 noStroke();
+ellipse(circle.x,circle.y,circle.size);
 
 }
