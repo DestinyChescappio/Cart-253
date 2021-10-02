eac1671c3f7c9bd043c9b9d20e6f06911edf5a5a
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-02 16:28:33 -0400

    finished functions exercise

--- Experiments/Functions/js/script.js
+++ Experiments/Functions/js/script.js
@@ -13,25 +13,21 @@ function setup() {
   //example of calling a function: it does whatever job it's supposed to do but we don't know how it's done
 
   createCanvas(500,500);
-
-
 }
-
-
 /**
 Description of draw()
 */
 function draw() {
   background(0);
-//circle movement
-circle.x = circle.x + circle.vx;
-circle.y = circle.y + circle.vy;
 
-//by calling the reset function, we're telling javascript to do the repeated instructions in the 'Function Reset' below
-//if the circle on the x axis
-if (circle.x > width) {
-  reset();
-}
+move();
+wrap();
+display();
+//circle movement; use 'function move' below instead of this
+//circle.x = circle.x + circle.vx;
+//circle.y = circle.y + circle.vy;
+
+
 //(instead of doing these 3 things, we can call our reset function ^^^)
 //if circle is reaching the right side,
 //if (circle.x > width){
@@ -42,21 +38,40 @@ if (circle.x > width) {
   //it would get bigger each time
   //circle.size = circle.size + 5;
 }
-//circle display
-fill(255,0,0);
-  ellipse(circle.x,circle.y,circle.size);
+//can also use the function move to call it somewhere else
+function move(){
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
 }
-//after mousepressed, we can call the function to reset
+
+  function wrap(){
+    if (circle.x > width){
+      reset();
+    }
+  }
+//circle display
+function display(){
+fill(255,0,0);
+ellipse(circle.x,circle.y,circle.size);
+}
+
+//after mousepressed, we can call the function to reset; this is also the main origin when calling the funtion 'reset'
 function reset(){
   circle.x = 0;
   circle.vx = circle.vx + 2;
+  circle.vy = circle.vy - 0.25;
   circle.size = circle.size + 5;
 
 }
-
+//we can do the same effect here. To use the reset function to repeat its actions instead of typing out the same thing over n over again
 function mousePressed(){
+  //so instead of writing this whole thing....
+
 //have the user reset the circle by clicking (this is a repeated code)
-circle.x = 0;
-circle.vx = circle.vx + 2;
-circle.size = circle.size + 5;
+//circle.x = 0;
+//circle.vx = circle.vx + 2;
+//circle.size = circle.size + 5;
+
+//just write:
+reset();
 }
