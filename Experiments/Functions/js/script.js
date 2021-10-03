fca81ce089c49eaeaa598e9efa1a3bd55dda726a
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-03 00:17:02 -0400

    finished functions with return values experiment
    Needs more insight and help. Especially with understanding the 'math' parts

--- Experiments/Functions/js/script.js
+++ Experiments/Functions/js/script.js
@@ -5,14 +5,20 @@ Description of setup
 function setup() {
 createCanvas(500,500);
 }
+
+let hotCelsius = toCelsius(100);
+console.log('100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.');
+
+let coldCelsius = toCelsius(10);
+console.log('10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.');
 /**
 Description of draw()
 */
 function draw() {
   background(0);
 
-  let x = random(0,width);
-  let y = random(0,height);
-
-  ellipse(x,y,100);
+}
+function toCelsius(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  return celsius;
 }
