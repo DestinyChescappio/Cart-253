5cc1d366361eb88e1b4987bec91e37a5eb3c7b47
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-03 23:49:46 -0400

    A5: setup up circle1 and circle2

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -13,19 +13,39 @@ Description of preload
 function preload() {
 
 }
+let circle1 {
+  x: undefined,
+  y: 167,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5
+}
+let circle2 = {
+  x: undefined,
+  y: 334,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5
+}
 
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
+  setupCircles();
+}
+function setupCircle() {
+  //position circles separated from one another
+  circle1.x = width / 3;
+  circle2.x = 2 * width / 3;
+
 
 }
 
 
-/**
-Description of draw()
-*/
-function draw() {
 
 }
