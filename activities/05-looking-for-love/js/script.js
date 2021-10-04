d4b61d48266fbe7b55e213f769705a2393635923
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-04 01:00:33 -0400

    A5: moved the movement code into a move function and call it

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -13,22 +13,23 @@ Description of preload
 function preload() {
 
 }
-let circle1 {
+let circle1 = {
   x: undefined,
-  y: 167,
+  y: 250,
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5
-}
+  speed: 3
+};
+
 let circle2 = {
   x: undefined,
-  y: 334,
+  y: 250,
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5
-}
+  speed: 3
+};
 
 
 /**
@@ -36,36 +37,45 @@ Description of setup
 */
 function setup() {
   createCanvas(500,500);
-  setupCircles();
-}
-function setupCircles() {
-  //position circles separated from one another
+//position circle separated from one another
   circle1.x = width / 3;
   circle2.x = 2 * width / 3;
-
 //start moving circles in random direction
 circle1.vx = random(-circle1.speed,circle1.speed);
 circle1.vy = random(-circle1.speed,circle1.speed);
+
 circle2.vx = random(-circle2.speed,circle2.speed);
 circle2.vy = random(-circle2.speed,circle2.speed);
 }
-}
+
 
 function draw() {
   background(0);
 
-function move() {
-circle1.x = circle1.x + circle1.vx;
-circle1.y = circle1.y + circle1.vy;
+move();
 
-circle2.x = circle2.x + circle2.vx;
-circle2.y = circle2.y + circle2.vy;
+//check if circles have gone offscreen
+if (circle1.x < 0 || circle1.x > width || circle1.y < 0 circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+  //sad ending
+}
+//check if circles overlaps
+let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+//if circles are touching
+if (d < circl1.size/2 + circl2.size/2) {
 
+  //love ending
 
-
+    //displaying circles
+  ellipse(circle1.x,circle1.y,circle1.size);
+  ellipse(circle2.x,circle2.y,circle2.size);
+  }
 }
 
-  //displaying circles
-ellipse(circle1.x,circle1.y,circle1.size);
-ellipse(circle2.x,circle2.y,circle2.size);
+function move() {
+  //moving circles
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
 }
