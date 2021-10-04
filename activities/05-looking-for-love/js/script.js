0cf8b9c04a00df8b3346e0bc9d5ce9a9e994cf92
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-03 23:31:15 -0400

    A5: started the Looking for Love activity

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -1,12 +1,10 @@
 /**
-Title of Project
-Author Name
+A5: Looking for Love
+Destiny Chescappio
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
-
-"use strict";
 
 
 /**
