dcfa5c87f400c9c8dd44eaa8fb413ddfa8c5f1e5
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-07 01:05:08 -0400

    p1: created a display function for ufo
    problem to be solved: why did my ufo objects change color?

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -43,6 +43,29 @@ let ufo1window = {
 speed: 1,
   fill: 0
 }
+let ufo2 = {
+x: 600,
+y: 200,
+w: 60,
+h: 15,
+speed: 1,
+stroke: 'rgba(100%,0%,100%,0.5)',
+strokeWeight: 3,
+fill: {
+  r: 0,
+  g: 255,
+  b: 0
+  }
+}
+let ufo2window = {
+  x: 600,
+  y: 195,
+  w: 20,
+  h: 13,
+speed: 1,
+  fill: 0
+}
+
 
 /**
 Description of setup
@@ -61,37 +84,58 @@ background(157); //for now bg
 imageMode(CENTER);
 image(galaxyImage,width / 2, height /2);
 
-  move();
-  display(ufo1);
+display(ufo1);
+display(ufo1window);
+display(ufo2);
+display(ufo2window);
+
+  //move();
+  //display(ufo1);
+  //display(ufo2);
 
 //Ufo1 movement setup
   ufo1.x = ufo1.x + ufo1.speed;
   ufo1window.x = ufo1window.x + ufo1window.speed;
 
 //if the ufo1.x exceeds the width of canvas and return to the left side
-function move(){
+//ufo1 going back and forth (starting LEFT side)
 if(ufo1.x > width) {
     ufo1.speed = -ufo1.speed;
   }
 if(ufo1window.x > width) {
     ufo1window.speed = -ufo1window.speed;
   }
+
+//ufo2 movement setup
+ufo2.x = ufo2.x + ufo2.speed;
+ufo2window.x = ufo2window.x + ufo2window.speed;
+//ufo2 going back and forth (starting RIGHT side)
+if(ufo2.x > width) {
+    ufo2.speed = -ufo2.speed;
+  }
+if(ufo2window.x > width) {
+    ufo2window.speed = -ufo2window.speed;
 }
 
+
 //ufo1 drawing using the green structure & ufo window
-function display() {
+function display(ufo) {
   //displaying ufo1 green structure
-  push();
   stroke('rgba(100%,0%,100%,0.5)');
   strokeWeight(3);
-  fill(ufo1.fill.r,ufo1.fill.g,ufo1.fill.b);
-  ellipse(ufo1.x,ufo1.y,ufo1.w,ufo1.h);
-  //display the ufo1 flashing window
-  ufo1window.fill = random(0,255);
-  fill(ufo1window.fill);
-  ellipse(ufo1window.x,ufo1window.y,ufo1window.w,ufo1window.h);
-  pop();
+  fill(ufo.fill.r,ufo.fill.g,ufo.fill.b);
+  ellipse(ufo.x,ufo.y,ufo.w,ufo.h);
 }
+  //display the ufo1 flashing window
+function display(ufowindow){
+  ufowindow.fill = random(0,255);
+  fill(ufowindow.fill);
+  ellipse(ufowindow.x,ufowindow.y,ufowindow.w,ufowindow.h);
+
+
+}
+//ufo2 display
+
 
 
 
