9d97b21d8fa2baaed7c3a6f7a4724ab48bb262e2
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-07 18:12:56 -0400

    p1: made function display, movement, bounce
    more neat :)

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -26,7 +26,8 @@ x: 0,
 y: 300,
 w: 60,
 h: 15,
-speed: 1,
+vx: 1,
+vy: 0,
 stroke: 'rgba(100%,0%,100%,0.5)',
 strokeWeight: 3,
 fill: {
@@ -35,20 +36,14 @@ fill: {
   b: 0
   }
 }
-let ufo1window = {
-  x: 0,
-  y: 295,
-  w: 20,
-  h: 13,
-speed: 1,
-  fill: 0
-}
+
 let ufo2 = {
 x: 600,
 y: 200,
 w: 60,
 h: 15,
-speed: 1,
+vx: -1,
+vy: 0,
 stroke: 'rgba(100%,0%,100%,0.5)',
 strokeWeight: 3,
 fill: {
@@ -57,14 +52,7 @@ fill: {
   b: 0
   }
 }
-let ufo2window = {
-  x: 600,
-  y: 195,
-  w: 20,
-  h: 13,
-speed: 1,
-  fill: 0
-}
+
 
 
 /**
@@ -85,74 +73,44 @@ imageMode(CENTER);
 image(galaxyImage,width / 2, height /2);
 
 display(ufo1);
-display(ufo1window);
 display(ufo2);
-display(ufo2window);
 
-  //move();
-  //display(ufo1);
-  //display(ufo2);
+movement(ufo1);
+movement(ufo2);
 
-//Ufo1 movement setup
-  ufo1.x = ufo1.x + ufo1.speed;
-  ufo1window.x = ufo1window.x + ufo1window.speed;
-
-//if the ufo1.x exceeds the width of canvas and return to the left side
-//ufo1 going back and forth (starting LEFT side)
-if(ufo1.x > width) {
-    ufo1.speed = -ufo1.speed;
-  }
-if(ufo1window.x > width) {
-    ufo1window.speed = -ufo1window.speed;
-  }
-
-//ufo2 movement setup
-ufo2.x = ufo2.x + ufo2.speed;
-ufo2window.x = ufo2window.x + ufo2window.speed;
-//ufo2 going back and forth (starting RIGHT side)
-if(ufo2.x > width) {
-    ufo2.speed = -ufo2.speed;
-  }
-if(ufo2window.x > width) {
-    ufo2window.speed = -ufo2window.speed;
-}
-
-
-//ufo1 drawing using the green structure & ufo window
-function display(ufo) {
-  //displaying ufo1 green structure
-  stroke('rgba(100%,0%,100%,0.5)');
-  strokeWeight(3);
-  fill(ufo.fill.r,ufo.fill.g,ufo.fill.b);
-  ellipse(ufo.x,ufo.y,ufo.w,ufo.h);
-}
-  //display the ufo1 flashing window
-function display(ufowindow){
-  ufowindow.fill = random(0,255);
-  fill(ufowindow.fill);
-  ellipse(ufowindow.x,ufowindow.y,ufowindow.w,ufowindow.h);
-
-
-}
-//ufo2 display
-
-
-
-
-
-
-
+bounce(ufo1);
+bounce(ufo2);
 
 //displaying blackhole
 fill(blackhole.fill);
 ellipse(blackhole.x,blackhole.y,blackhole.size);
 
+}
+function movement(ufo){
+//Ufo movement setup
+ufo.x = ufo.x + ufo.vx;
+ufo.y = ufo.y + ufo.vy;
+}
 
+//ufo drawing using the green structure & ufo window
+function display(ufo) {
+  //ufo green structure
+stroke('rgba(100%,0%,100%,0.5)');
+strokeWeight(3);
+fill(ufo.fill.r,ufo.fill.g,ufo.fill.b);
+ellipse(ufo.x,ufo.y,ufo.w,ufo.h);
+//window
+fill(random(0,255));
+ellipse(ufo.x,ufo.y - 5, 20, 13);
+}
 
-
-
-
-
-
-
+function bounce(ufo) {
+//ufo starting at LEFT side going to RIGHT side
+if(ufo1.x > width || ufo1.x < 0) {
+ufo1.vx = -ufo1.vx;
+  }
+//ufo starting at RIGHT side going to LEFT side
+if(ufo2.x > width || ufo2.x < 0) {
+ufo2.vx = -ufo2.vx;
+    }
 }
