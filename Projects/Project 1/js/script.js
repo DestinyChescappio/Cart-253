f77a4d9162cacfcb034184faf4a4c7eb1b0cf24d
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-12 17:22:45 -0400

    p1: organized comments

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -15,40 +15,40 @@ let blackhole = {
   x: 500,
   y: 500,
   size: 100,
-  fill:0
+  fill: 0
 }
 
 let ufo1 = {
-x: 0,
-y: 300,
-w: 60,
-h: 15,
-vx: 3,
-vy: 0,
-stroke: 'rgba(100%,0%,100%,0.5)',
-strokeWeight: 3,
+  x: 0,
+  y: 300,
+  w: 60,
+  h: 15,
+  vx: 3,
+  vy: 0,
+  stroke: 'rgba(100%,0%,100%,0.5)',
+  strokeWeight: 3,
   beingDragged: false,
-fill: {
-  r: 0,
-  g: 255,
-  b: 0
+  fill: {
+    r: 0,
+    g: 255,
+    b: 0
   }
 }
 
 let ufo2 = {
-x: 600,
-y: 200,
-w: 60,
-h: 15,
-vx: -3,
-vy: 0,
-stroke: 'rgba(100%,0%,100%,0.5)',
-strokeWeight: 3,
+  x: 600,
+  y: 200,
+  w: 60,
+  h: 15,
+  vx: -3,
+  vy: 0,
+  stroke: 'rgba(100%,0%,100%,0.5)',
+  strokeWeight: 3,
   beingDragged: false,
-fill: {
-  r: 0,
-  g: 255,
-  b: 0
+  fill: {
+    r: 0,
+    g: 255,
+    b: 0
   }
 }
 let ufo3 = {
@@ -65,7 +65,7 @@ let ufo3 = {
     r: 0,
     g: 255,
     b: 0
- }
+  }
 }
 let ufo4 = {
   x: 600,
@@ -76,13 +76,14 @@ let ufo4 = {
   vy: 0,
   stroke: 'rgba(100%,0%,100%,0.5)',
   strokeWeight: 3,
-    beingDragged: false,
+  beingDragged: false,
   fill: {
     r: 0,
     g: 255,
     b: 0
   }
 }
+
 function preload() {
   galaxyImage = loadImage('assets/images/galaxy.jpeg');
 }
@@ -92,7 +93,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(600,600);
+  createCanvas(600, 600);
 
 }
 
@@ -100,31 +101,43 @@ function setup() {
 Description of draw()
 */
 function draw() {
-background(157); //for now bg
+  background(157); //for now bg
 
-if (state ===`simulation`) {
+  if (state === `simulation`) {
     simulation();
+  } else if (state === `ending`) {
+    ending();
   }
-else if (state === `ending`) {
-ending();
-}
 
 }
-function simulation(){
+
+function simulation() {
   imageMode(CENTER);
-  image(galaxyImage,width / 2, height /2);
+  image(galaxyImage, width / 2, height / 2);
 
-if(ufoIsInsideHole(ufo1)){
-  //stops moving
+  if (!ufoIsInsideHole(ufo1)) {
+    movement(ufo1);
+  }
+  if (!ufoIsInsideHole(ufo2)) {
+    movement(ufo2);
+  }
+  if (!ufoIsInsideHole(ufo3)) {
+    movement(ufo3);
+  }
+  if (!ufoIsInsideHole(ufo4)) {
+    movement(ufo4);
+  }
 
-}else{
-  //starts moving
-  movement(ufo1);
+//to check if ALL ufos are in black ufo
+if (ufoIsInsideHole(ufo1)&& ufoIsInsideHole(ufo2)&& ufoIsInsideHole(ufo3)&& ufoIsInsideHole(ufo4)){
+state = `ending`;  
 }
+
+
 
   //displaying blackhole
   fill(blackhole.fill);
-  ellipse(blackhole.x,blackhole.y,blackhole.size);
+  ellipse(blackhole.x, blackhole.y, blackhole.size);
 
   mouseIsInsideUfo(ufo1);
   mouseIsInsideUfo(ufo2);
@@ -149,73 +162,73 @@ if(ufoIsInsideHole(ufo1)){
   rightbounce(ufo2);
 
 }
-function movement(ufo){
-//Ufo movement setup
+
+function movement(ufo) {
+  //Ufo movement setup
   ufo.x = ufo.x + ufo.vx;
   ufo.y = ufo.y + ufo.vy;
-let d = dist(ufo1.x,ufo1.y, )
-  }
+  let d = dist(ufo1.x, ufo1.y, )
+}
 
 //dragging ufo
-function handleDragging(ufo){
-  if (ufo.beingDragged === true){
+function handleDragging(ufo) {
+  if (ufo.beingDragged === true) {
     ufo.x = mouseX;
     ufo.y = mouseY;
   }
 }
 
-function mouseIsInsideUfo(ufo){
-if (mouseX > ufo.x - ufo.w / 2 && mouseX < ufo.x + ufo.w /2 &&
-mouseY > ufo.y - ufo.h /2 && mouseY < ufo.y + ufo.h / 2){
+function mouseIsInsideUfo(ufo) {
+  if (mouseX > ufo.x - ufo.w / 2 && mouseX < ufo.x + ufo.w / 2 &&
+    mouseY > ufo.y - ufo.h / 2 && mouseY < ufo.y + ufo.h / 2) {
 
 
-      console.log("true");
-      return true;
-    } else {
-      return false;
-    }
-}
-
-function mousePressed(){
-if (mouseIsInsideUfo(ufo1)) {
-ufo1.beingDragged = true;
-}
-if (mouseIsInsideUfo(ufo2)) {
-ufo2.beingDragged = true;
-}
-if (mouseIsInsideUfo(ufo3)) {
-ufo3.beingDragged = true;
-}
-if (mouseIsInsideUfo(ufo4)) {
-ufo4.beingDragged = true;
-}
+    console.log("true");
+    return true;
+  } else {
+    return false;
   }
-
-function mouseReleased(){
-if (ufo1.beingDragged){
-ufo1.beingDragged = false;
-}
-if (ufo2.beingDragged){
-ufo2.beingDragged = false;
-}
-if (ufo3.beingDragged){
-ufo3.beingDragged = false;
-}
-if (ufo4.beingDragged){
-ufo4.beingDragged = false;
-}
 }
 
-function ufoIsInsideHole(ufo){
-//check if ufo1 is inside hole
-let d = dist(ufo.x, ufo.y, blackhole.x, blackhole.y);
-//return true if distance is less than balckhole radius
-if(d < blackhole.size / 2){
-return true;
-}else{
-return false;
+function mousePressed() {
+  if (mouseIsInsideUfo(ufo1)) {
+    ufo1.beingDragged = true;
+  }
+  if (mouseIsInsideUfo(ufo2)) {
+    ufo2.beingDragged = true;
+  }
+  if (mouseIsInsideUfo(ufo3)) {
+    ufo3.beingDragged = true;
+  }
+  if (mouseIsInsideUfo(ufo4)) {
+    ufo4.beingDragged = true;
+  }
 }
 
+function mouseReleased() {
+  if (ufo1.beingDragged) {
+    ufo1.beingDragged = false;
+  }
+  if (ufo2.beingDragged) {
+    ufo2.beingDragged = false;
+  }
+  if (ufo3.beingDragged) {
+    ufo3.beingDragged = false;
+  }
+  if (ufo4.beingDragged) {
+    ufo4.beingDragged = false;
+  }
+}
+
+function ufoIsInsideHole(ufo) {
+  //check if ufo1 is inside hole
+  let d = dist(ufo.x, ufo.y, blackhole.x, blackhole.y);
+  //return true if distance is less than balckhole radius
+  if (d < blackhole.size / 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
@@ -224,24 +237,24 @@ function display(ufo) {
   //displaying ufo green structure
   stroke('rgba(100%,0%,100%,0.5)');
   strokeWeight(3);
-  fill(ufo.fill.r,ufo.fill.g,ufo.fill.b);
-  ellipse(ufo.x,ufo.y,ufo.w,ufo.h);
+  fill(ufo.fill.r, ufo.fill.g, ufo.fill.b);
+  ellipse(ufo.x, ufo.y, ufo.w, ufo.h);
 
-//window
-  fill(random(0,255));
-  ellipse(ufo.x,ufo.y - 5, 20, 13);
+  //window
+  fill(random(0, 255));
+  ellipse(ufo.x, ufo.y - 5, 20, 13);
 }
 
 function leftbounce(ufo) {
-//ufo going back and forth (starting LEFT side)
-if(ufo.x > width || ufo.x < 0) {
+  //ufo going back and forth (starting LEFT side)
+  if (ufo.x > width || ufo.x < 0) {
     ufo.vx = -ufo.vx;
   }
 }
 
 function rightbounce(ufo) {
-//ufo going back and forth (starting RIGHT side)
-if(ufo.x > width || ufo.x < 0) {
-      ufo.vx = -ufo.vx;
+  //ufo going back and forth (starting RIGHT side)
+  if (ufo.x > width || ufo.x < 0) {
+    ufo.vx = -ufo.vx;
   }
 }
