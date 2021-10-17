d1c919258d677d93be9c4798969bd9d9cc5a2532
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-17 02:18:43 -0400

    finished debugging conditionals

--- Experiments/arrays/debugging conditionals/js/script.js
+++ Experiments/arrays/debugging conditionals/js/script.js
@@ -26,8 +26,21 @@ Description of draw()
 */
 function draw() {
   background(0);
+//synthax error: if we are missing both parenthesis '()' it will not work
+//in javaScript developer, it will show "unexpected identifier" because it always expects to see and opening parenthesis
+//the next thing the js developer it sees here is actually mouseX (that's unexpected) and an error.
 
-  if (mouseX < width/2){
+//we might also forget to close the parenthesis around our conditional so it will say: unexpected toke '{'
+//when we forget to open the curly brackets, it will say: unexpected token 'else'
+//this means the line our 'else' is on, which is not the problem, so we would need to move upward to see the problem
+
+//another error we can accidently write a condition that is always true (when we don't want that)
+//take this for example (our circle is always red because we wrote a condition that CAN'T be false)
+  if (mouseX >= 0){  //we CANNOT move the mouse so that mouseX is less than 0
+                    //and so the 'else' of the 'if' statement can NEVER happen
+    fill(255,0,0);
+  }
+  else {
     fill(0,255,0);
   }
   ellipse(circle.x,circle.y,circle.size);
