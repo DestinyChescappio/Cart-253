14eaf99c567e445c94ce9dc86c813cab15c723e9
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-18 00:28:20 -0400

    drew and displayed a fly and the squisher

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -8,7 +8,19 @@ author, and this description to match your project!
 
 "use strict";
 
-function createFlies(x, y) {
+//the user displayed as a fly swatter
+let squisher = {
+  x: 0,
+  y: 0,
+  size: 50,
+  fill: {
+    r: 0,
+    g: 255,
+    b: 0
+  }
+}
+
+//function createFlies(x, y) {
   let flies = {
   x: 300,
   y: 300,
@@ -19,8 +31,8 @@ function createFlies(x, y) {
   speed: 2,
   fill: 0
 }
-return flies;
-}
+//return flies;
+//}
 /**
 Description of preload
 */
@@ -36,10 +48,41 @@ function setup() {
 createCanvas(600,600);
 }
 
+function draw(){
+  background(150);
 
-/**
-Description of draw()
-*/
-function draw() {
+//drawing flies' bodies and wings
+  createFlies();
 
+//drawing book to squash flies
+ createSquisher();
+
+//using the book as the user position
+moveSquisher();
+}
+
+//sets the user position to the book position
+function moveSquisher() {
+squisher.x = mouseX;
+squisher.y = mouseY;
+}
+
+//drawing 3 ellipses to create the flies
+function createFlies() {
+background(150);
+fill(flies.fill);
+ellipse(flies.x,flies.y,flies.w,flies.h);
+//left wing
+fill(255);
+ellipse(flies.x-10,flies.y-5,10,5);
+//right wing
+fill(255);
+ellipse(flies.x+10,flies.y-5,10,5);
+}
+
+function createSquisher(){
+//drawing book to squash flies
+fill(squisher.fill.r,squisher.fill.g,squisher.fill.b);
+noStroke();
+ellipse(squisher.x,squisher.y,squisher.size);
 }
