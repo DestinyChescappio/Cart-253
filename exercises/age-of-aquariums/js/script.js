9a2b0866795a41424ce94b3b29895448072080a9
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-18 00:48:16 -0400

    created a new javascript object: the moving flies

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -8,29 +8,32 @@ author, and this description to match your project!
 
 "use strict";
 
+let fliesGang = [];
+let fliesGangSize = 20;
+
 //the user displayed as a fly swatter
-let squisher = {
-  x: 0,
-  y: 0,
-  size: 50,
-  fill: {
-    r: 0,
-    g: 255,
-    b: 0
-  }
-}
+//let squisher = {
+//  x: 0,
+//  y: 0,
+//  size: 50,
+//  fill: {
+//    r: 0,
+//    g: 255,
+//    b: 0
+//  }
+//}
 
 //function createFlies(x, y) {
-  let flies = {
-  x: 300,
-  y: 300,
-  h: 30,
-  w: 15,
-  vx: 0,
-  vy: 0,
-  speed: 2,
-  fill: 0
-}
+  //let flies = {
+//  x: 300,
+//  y: 300,
+//  h: 30,
+//  w: 15,
+//  vx: 0,
+//  vy: 0,
+//  speed: 2,
+//  fill: 0
+//}
 //return flies;
 //}
 /**
@@ -46,6 +49,10 @@ Description of setup
 */
 function setup() {
 createCanvas(600,600);
+
+for (let i = 0; i < flyGang; i++) {
+  let flies = createFlies(random(0,width), random(0,height));
+}
 }
 
 function draw(){
@@ -59,6 +66,9 @@ function draw(){
 
 //using the book as the user position
 moveSquisher();
+
+//check whether the user (squisher) has squished the fly
+checkFlies();
 }
 
 //sets the user position to the book position
@@ -67,17 +77,46 @@ squisher.x = mouseX;
 squisher.y = mouseY;
 }
 
+function checkFlies(flies) {
+  //check for an overlap if flies hasn't been squished by squisher
+  if (!flies.squished) {
+    let d = dist(squisher.x,squisher.y,flies.x,flies.y);
+    if (d < squisher.size / 2 + flies.size / 2) {
+      flies.squished = true;
+    }
+  }
+}
+
+function moveFlies(flies) {
+//moving the flies
+  flies.x = flies.x + flies.vx;
+  flies.y = flies.y + flies.vy;
+}
+
 //drawing 3 ellipses to create the flies
-function createFlies() {
-background(150);
-fill(flies.fill);
-ellipse(flies.x,flies.y,flies.w,flies.h);
+function createFlies(x, y) {
+  let flies = {
+  //flies body
+ x: 300,
+ y: 300,
+ fill: 0,
+ h: 30,
+ w: 15,
+ vx: 0,
+ vy: 0,
+ speed: 2
 //left wing
 fill(255);
 ellipse(flies.x-10,flies.y-5,10,5);
 //right wing
 fill(255);
 ellipse(flies.x+10,flies.y-5,10,5);
+}
+return flies;
+}
+
+function draw(){
+  background(150);
 }
 
 function createSquisher(){
