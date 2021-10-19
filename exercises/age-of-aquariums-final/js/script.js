98191722d1419bee3a5e03155b2291be0e43cf9d
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-19 16:28:36 -0400

    E4: made mouse body (tail is to be done)

--- exercises/age-of-aquariums-final/js/script.js
+++ exercises/age-of-aquariums-final/js/script.js
@@ -8,6 +8,17 @@ author, and this description to match your project!
 
 "use strict";
 
+let mouse = {
+x:300,
+y:300,
+h:30,
+w:50,
+size:100,
+vx:0,
+vy:0,
+fill:150
+}
+
 
 /**
 Description of setup
@@ -21,14 +32,23 @@ createCanvas(600,600);
 Description of draw()
 */
 function draw() {
+background(0);
+//mice
+displayMouse();
 
 }
 
-function createMice();
-let mice = {
-x:
-y:
-h:
-w:
-fill:  
+function displayMouse() {
+fill(150);
+noStroke();
+ellipse(mouse.x,mouse.y,mouse.h,mouse.w,mouse.size);
+//left ear
+fill(150);
+ellipse(mouse.x-20,mouse.y-10,mouse.h-10,mouse.w-30);
+//right ear
+fill(150);
+ellipse(mouse.x+20, mouse.y-10,mouse.h-10,mouse.w-30);
+//tail
+stroke(255);
+line(mouse.x,mouse.y,mouse.h+20,400,400); //needs to be in the middle vertically
 }
