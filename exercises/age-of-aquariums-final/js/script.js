275c8df4e994bc2693325ef5fae7a9fa046fd8a8
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-19 17:23:30 -0400

    E4: created cheese

--- exercises/age-of-aquariums-final/js/script.js
+++ exercises/age-of-aquariums-final/js/script.js
@@ -17,7 +17,12 @@ vx:0,
 vy:0,
 fill:150
 }
+let cheese = {
+x:undefined,
+y:undefined,
+size:65,
 
+}
 
 
 
@@ -34,12 +39,17 @@ Description of draw()
 */
 function draw() {
 background(0);
-//mice
+//mice display
 displayMouse();
+//cheese display
+displayCheese();
+//user cheese movement
+userCheese();
 
 }
 
 function displayMouse() {
+push();
 fill(150);
 noStroke();
 ellipse(mouse.x,mouse.y,mouse.h,mouse.w);
@@ -52,5 +62,30 @@ ellipse(mouse.x+20, mouse.y-17,mouse.h-10,mouse.w-30);
 //tail
 stroke('#fae');
 strokeWeight(3);
-line(mouse.x,mouse.y+25,mouse.x,mouse.y+mouse.h+20); //needs to be in the middle vertically
+line(mouse.x,mouse.y+25,mouse.x,mouse.y+mouse.h+20);
+pop(); //needs to be in the middle vertically
+}
+
+function displayCheese() {
+  let c = color(255,204,0);
+  fill(c);
+  noStroke();
+  ellipse(cheese.x,cheese.y,cheese.size);
+  //cheese hole1
+  fill(255,160,0);
+  ellipse(cheese.x-15,cheese.y-10,cheese.size-50);
+  //cheese hole2
+  fill(255,160,0);
+  ellipse(cheese.x-5,cheese.y+18,cheese.size-40);
+  //cheese hole3
+  fill(255,160,0);
+  ellipse(cheese.x+10,cheese.y-14,cheese.size-45);
+  //cheese hole4
+  fill(255,160,0);
+  ellipse(cheese.x+17,cheese.y+12,cheese.size-55);
+  }
+
+function userCheese() {
+  cheese.x = mouseX;
+  cheese.y = mouseY;
 }
