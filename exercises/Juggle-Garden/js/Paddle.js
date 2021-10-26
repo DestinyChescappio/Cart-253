d26eef1b7b17fdff3fb0b26319ccb6dc346ab15d
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-26 12:15:07 -0400

    E5: made paddle in display (key pressed should be done for the movement)

--- exercises/Juggle-Garden/js/Paddle.js
+++ exercises/Juggle-Garden/js/Paddle.js
@@ -4,5 +4,17 @@ class Paddle {
     this.height = h;
     this.x = 0;
     this.y = height - this.height/2;
-  }
+}
+move() {
+
+}
+
+display() {
+  push();
+  fill(255);
+  noStroke();
+  rectMode(CENTER);
+  rect(this.x,this.y,this.width,this.height);
+  pop();
+}
 }
