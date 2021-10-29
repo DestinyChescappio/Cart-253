9b162130e13280e9af88312f590f11e87f699ae4
DestinyChescappio <chescappiodestiny@gmail.com>
2021-10-29 01:27:06 -0400

    Started Inheritance Activity

---
+++ activities/Inheritance-Activity/js/script.js
@@ -0,0 +1,33 @@
+/**
+Inheritance Activity
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
