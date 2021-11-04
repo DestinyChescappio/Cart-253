fae5292768099a6853b41eaa7c1320bb7b750e96
DestinyChescappio <chescappiodestiny@gmail.com>
2021-11-04 18:21:30 -0400

    started PolySynth

---
+++ Experiments/polySynth/js/script.js
@@ -0,0 +1,27 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+let synth;
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(600,600);
+  userStartAudio();
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(0);
+}
