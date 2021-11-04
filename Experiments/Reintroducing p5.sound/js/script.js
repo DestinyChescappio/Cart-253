fe5c7794a6f6180097baf0d55efd1df72feb36ec
DestinyChescappio <chescappiodestiny@gmail.com>
2021-11-04 16:27:28 -0400

    started p5.Oscillator

--- Experiments/Reintroducing p5.sound/js/script.js
+++ Experiments/Reintroducing p5.sound/js/script.js
@@ -8,16 +8,6 @@ author, and this description to match your project!
 
 "use strict";
 
-let barkSFX;
-
-/**
-Description of preload
-*/
-function preload() {
-barkSFX = loadSound(`assets/sounds/bark.wav`);
-}
-
-
 /**
 Description of setup
 */
@@ -33,10 +23,8 @@ Description of draw()
 function draw() {
 background(0);
 
-let newRate = map(mouseX,0,width,-3,3); //makes the dog bark at low or high voice when clicked but also continue barking as the mouse moves
-barkSFX.rate(newRate);
 }
 
 function mousePressed() {
-  barkSFX.play();
+
 }
