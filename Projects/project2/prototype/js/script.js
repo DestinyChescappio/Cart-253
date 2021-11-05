87fc2c39e8669982863d77e2d6f264614424a1c5
DestinyChescappio <chescappiodestiny@gmail.com>
2021-11-05 15:29:05 -0400

    P2: started prototype

---
+++ Projects/project2/prototype/js/script.js
@@ -0,0 +1,23 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+//
+function setup() {
+createCanvas(800,600);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+background(0);
+}
