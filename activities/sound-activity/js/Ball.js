178458bfda35313939d6bf28cb3b26563211b2b8
DestinyChescappio <chescappiodestiny@gmail.com>
2021-11-06 17:11:34 -0400

    added a constructor

---
+++ activities/sound-activity/js/Ball.js
@@ -0,0 +1,24 @@
+class Ball {
+//setting the constructor's properties
+  constructor(x,y){
+  this.x = x;
+  this.y = y;
+  this.size = 50;
+  this.fill = random(150,0,255);
+  this.speed = 5;
+  this.vx = random(-this.speed,this.speed);
+  this.vy = random(-this.speed,this.speed);
+  }
+}
+
+move(){
+  
+}
+
+bounce(){
+
+}
+
+display(){
+
+}
