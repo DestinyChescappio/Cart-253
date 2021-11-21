d61fdf8f1dca07e3477ac45dc75a665bd925376a
DestinyChescappio <chescappiodestiny@gmail.com>
2021-11-21 16:06:11 -0500

    p2: made a head color property & worked on the display property

--- Projects/project2/project2/js/Kid.js
+++ Projects/project2/project2/js/Kid.js
@@ -2,12 +2,18 @@ class Kids {
   constructor() {
     this.x = x;
     this.y = y;
-    this.size = 40;
+    this.width = 15;
+    this.height = 25;
     this.vy = 0;
     this.vx = 4;
     this.speed = 4;
     //kidColor properties are located in the main script
     this.kidColor = kidColor;
+    this.headColor = {
+      r: 245,
+      g: 245,
+      b: 220,
+    };
   }
 
   move() {
@@ -24,7 +30,13 @@ class Kids {
   display() {
     push();
     noStroke();
-    fill();
+    fill(this.kidColor.r, this.kidColor.g, this.kidColor.b);
+    //face
+
+    //head
+    ellipse(this.x, this.y, this.width, this.height - 10);
+    //body
+    ellipse(this.x, this.y, this.width, this.height);
     pop();
   }
 }
